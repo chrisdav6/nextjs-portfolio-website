e8965f1e657b934a4ac47fa0c8c7884d39e77919
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-08-08 10:47:26 -0400

    Created About component

---
+++ components/SectionHeading.tsx
@@ -0,0 +1,9 @@
+type SectionHeadingProps = {
+  text: string;
+};
+
+const SectionHeading = ({ text }: SectionHeadingProps) => {
+  return <h2 className='text-3xl font-medium mb-8'>{text}</h2>;
+};
+
+export default SectionHeading;
