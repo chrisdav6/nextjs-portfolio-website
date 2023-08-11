1dbb5d22d98c7d82f906fc2528613966d38bec82
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-08-11 15:10:28 -0400

    Fine tuned responsiveness

--- components/Project.tsx
+++ components/Project.tsx
@@ -21,9 +21,9 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
     <motion.section
       ref={ref}
       style={{ scale: scaleProgress, opacity: opacityProgress }}
-      className='group bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden relative mb-3 cursor-pointer sm:pr-8 sm:h-[20rem] sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20'
+      className='group bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden relative mb-3 cursor-pointer sm:pr-8 sm:h-[20rem] sm:mb-8 last:mb-0 sm:even:pl-8 hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20'
     >
-      <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
+      <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]'>
         <h3 className='text-2xl font-semibold'>{title}</h3>
         <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>
           {description}
@@ -45,7 +45,7 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
         src={imageUrl}
         alt={title}
         quality={95}
-        className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 transition'
+        className='hidden sm:block absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 transition'
       />
     </motion.section>
   );
